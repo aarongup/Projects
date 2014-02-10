b6a9f67f916f5dceec7725296593d195792b08da
aarongu <aarongu@uw.edu>
2014-02-09 22:18:58 -0800

    Update Rock, Paper, Scissors, Lizard , Spock.js
    Added option to continue to play. Minor bug fixes regarding inputs

--- Rock, Paper, Scissors, Lizard , Spock.js
+++ Rock, Paper, Scissors, Lizard , Spock.js
@@ -4,6 +4,9 @@
 
 
 
+var continueToPlay = 1;
+while(continueToPlay = 1) {
+	
 alert("Welcome to the rock-paper-lizard-spock game inspired by the show Big Bang Theory!");
 
 var userChoice = prompt("Do you choose rock, paper, scissors, lizard, or spock?");
@@ -20,56 +23,57 @@ if (computerChoice <= 0.2) {
     computerChoice = "spock";
 }
 
-while (userChoice !== "rock" && userChoice !== "paper" && userChoice !== "scissors" && userChoice !== "lizard" && userChoice !=="spock") {
+userChoiceLC = userChoice.toLowerCase();
+while (userChoiceLC !== "rock" && userChoiceLC !== "paper" && userChoiceLC !== "scissors" && userChoiceLC !== "lizard" && userChoiceLC !=="spock") {
     alert("Damn it, pick a right one!");
-    userChoice = prompt("Do you choose rock, paper, scissors, lizard, or spock?");
+    userChoiceLC = prompt("Do you choose rock, paper, scissors, lizard, or spock?");
 }
-
+userChoice = userChoice.toLowerCase();
 alert("You have chosen " + userChoice + " and the computer has chosen " + computerChoice);
 
 var compare = function(choice1, choice2) {
-    if (choice1 === choice2) {
+    if (choice1 == choice2) {
         return "The result is a tie!";
     }
-    else if (choice1 === "rock") {
-        if (choice2 === "scissors") {
+    else if (choice1 == "rock") {
+        if (choice2 == "scissors") {
             return "rock crushes scissors! You win!";
-        } else if (choice2 === "paper") {
+        } else if (choice2 == "paper") {
             return "paper covers rock. Lol you lost";
-        } else if (choice2 === "lizard") {
+        } else if (choice2 == "lizard") {
             return "rock crushes lizard! You win!";
         } else  {
             return "spock vaporizes rock. Trololol you lost";
         }
     }
-    else if (choice1 === "paper") {
-        if (choice2 === "rock") {
+    else if (choice1 == "paper") {
+        if (choice2 == "rock") {
             return "paper covers rock. You win!";
         } else if (choice2 == "scissors") {
             return "scissors cuts paper. You lose.";
-        } else if (choice2 === "lizard") {
+        } else if (choice2 == "lizard") {
             return "lizard eats paper. You lose";
         } else {
             return "paper disproves spock. You win";
         }
     }
-    else if (choice1 === "scissors") {
-        if (choice2 === "rock") {
+    else if (choice1 == "scissors") {
+        if (choice2 == "rock") {
             return "rock crushes scissors. You lose";
         } else if (choice2 == "paper") {
             return "scissors cuts paper. You win.";
-        } else if (choice2 === "lizard") {
+        } else if (choice2 == "lizard") {
             return "scissors decapitates lizard. You win";
         } else {
             return "spock smashes scissors. You lose";
         }
     }
-    else if (choice1 === "lizard") {
-        if (choice2 === "rock") {
+    else if (choice1 == "lizard") {
+        if (choice2 == "rock") {
             return "rock crushes lizard.You lose";
         } else if (choice2 == "paper") {
             return "lizard eats paper. You win.";
-        } else if (choice2 === "scissors") {
+        } else if (choice2 == "scissors") {
             return "scissors decapitates lizard. You lose";
         } else {
             return "Lizard poisons spock. You win";
@@ -78,9 +82,9 @@ var compare = function(choice1, choice2) {
     else {
         if(choice2 == "rock") {
             return "spock vaporizes rock. You win";
-        } else if(choice2 === "paper") {
+        } else if(choice2 == "paper") {
             return "paper disproves spock.You lose";
-        } else if(choice2 === "scissors") {
+        } else if(choice2 == "scissors") {
             return "spock smashes scissors. You win";
         } else  {
             return "lizard poisons spock. You lose";
@@ -89,3 +93,16 @@ var compare = function(choice1, choice2) {
 }
 
 alert(compare(userChoice,computerChoice))
+
+var decisionToContinue = prompt("Would you like to continue to play, yes or no ");
+
+while (decisionToContinue.toLowerCase() !== 'yes' && decisionToContinue.toLowerCase() !== 'no') {
+	decisionToContinue = prompt("Invalid response. yes or not?")
+}
+if(decisionToContinue.toLowerCase() === 'yes') {
+	continueToPlay = true;
+} else {
+	continueToPlay = false;
+}
+
+}
